abf49e30e1cacce9ff4a326f363464cb16eb429e
Timur <Timur@DESKTOP-ETUOQI3>
2022-07-04 07:37:12 +0700

    Добавьте файлы проекта.

---
+++ c-sharp-game/test.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes.Data;
+
+Console.WriteLine("Hello, World!");
+string test = "test";
+Console.WriteLine(test);
+
+Person Timur = new Person();
+Timur.name = "Timur";
+Timur.age = 22;
+Timur.Print();
+
+
+
